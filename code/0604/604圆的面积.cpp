a68e3a5d00cf1cbbc77f08b24afe8c4b88e5875b
zyz <2718629413@qq.com>
2023-03-04 23:41:46 +0800

    add some syntax problem code

---
+++ code/0604/604圆的面积.cpp
@@ -0,0 +1,13 @@
+#include <algorithm>
+#include <cstring>
+#include <iostream>
+using namespace std;
+
+int main() {
+  const double pi = 3.14159;
+  double r;
+  cin >> r;
+  printf("A=%.4lf", pi * r * r);
+
+  return 0;
+}
