89bab1477e16dcb1bfa13b8f6aaa9c27dda19acf
Kyrie <61409791+kyriekevin@users.noreply.github.com>
2023-10-23 00:09:02 -0500

    add offer lesson notes

---
+++ 14.不修改数组找出重复的数字.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+  int duplicateInArray(vector<int> &nums) {
+    int l = 1, r = nums.size() - 1;
+    while (l < r) {
+      int mid = l + r >> 1;
+      int s = 0;
+      for (auto num : nums)
+        s += num >= l && num <= mid;
+      if (s > mid - l + 1)
+        r = mid;
+      else
+        l = mid + 1;
+    }
+
+    return l;
+  }
+};
